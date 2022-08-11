599fb935a778034b06eadbefe362ce2884591d37
volant360 <efrainh12@galileo.edu>
2022-08-11 13:54:45 -0600

    refactor: refactored http plugin

--- vue/plugins/http.js
+++ vue/plugins/http.js
@@ -1,7 +1,45 @@
+/*
+@description HTTP plugin for all components. Allows apps to 
+  make http requests using axios
+*/
 import axios from "axios";
 
-//  · Plugin initializing
-// ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// @param response [Object] The response object as returned by axios
+// @return [Object] An object containing the status of the response and
+//   any data/error that was received
+const responseHandler = (response) => {
+  if (response.status == 200) {
+    return {
+      data: response.data.data,
+      status: response.status,
+    };
+  }
+
+  return {
+    status: response.status,
+    error: response.data.error,
+  };
+};
+
+// @param error [Object] The error that ocurred when rending an HTTP request
+// @return [Object] An object containing the error code and message 
+const errorHandler = (error) => {
+  let message = "";
+  if (error.response.data.error && error.response.data.error.message) {
+    message = error.response.data.error.message;
+  } else {
+    message = `HTTP Request ${error.config.method} to url ${error.config.url} ${error.message}`;
+  }
+
+  return {
+    status: error.response.status,
+    error: {
+      error: error.response.statusText,
+      message: message,
+    },
+  };
+};
+
 export default {
   install(Vue, _options) {
     // Get authentication token from rails
@@ -12,41 +50,12 @@ export default {
       token = meta.getAttribute("content");
     }
 
+    // Add the authentication token from rails
     let http = axios.create({
       headers: { "X-CSRF-Token": token },
     });
 
-    http.interceptors.response.use(
-      (response) => {
-        if (response.status == 200) {
-          return {
-            data: response.data.data,
-            status: response.status
-          };
-        }
-
-        return {
-          status: response.status,
-          error: response.data.error,
-        };
-      },
-      (error) => {
-        let message = "";
-        if (error.response.data.error && error.response.data.error.message) {
-          message = error.response.data.error.message;
-        } else {
-          message = `HTTP Request ${error.config.method} to url ${error.config.url} ${error.message}`;
-        }
-
-        return {
-          status: error.response.status,
-          error: {
-            error: error.response.statusText,
-            message: message,
-          },
-        };
-      }
-    );
+    http.interceptors.response.use(responseHandler, errorHandler);
     Vue.prototype.http = http;
   },
 };
