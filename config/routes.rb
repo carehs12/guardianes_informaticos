c92852711bece6a2c8984c231a01baf219637bf9
volant360 <efrainh12@galileo.edu>
2022-08-10 19:36:20 -0600

    featL added rubocop config and devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for(
+    :users,
+    controllers: {
+      passwords: 'users/passwords',
+      sessions: 'users/sessions'
+    },
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'password'
+    }
+  )
 end
