f3dd7a94381e7e63954602872dbb579c16e30d8b
volant360 <efrainh12@galileo.edu>
2022-08-11 22:34:37 -0600

    feat: added methods for model user

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
+# Base model from which all other models will inherit
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.paginate_records(total_records, records)
+    {
+      total_records: total_records,
+      records: records
+    }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# Manages users in the system
 class User < ApplicationRecord
   devise(:database_authenticatable, :validatable, :recoverable, authentication_keys: [:username])
 
@@ -12,4 +13,35 @@ class User < ApplicationRecord
 
   # Validations
   validates :username, presence: true, uniqueness: true
+
+  def self.index(query, page = 0, per_page = 100)
+    data = joins(:detail)
+
+    unless query.blank?
+      subquery1 = data.where('username like ?', "%#{query}%")
+      subquery2 = data.where('first_names like ?', "%#{query}%")
+      subquery3 = data.where('last_names like ?', "%#{query}%")
+
+      data = data.where('email like ?', "%#{query}%").or(subquery1).or(subquery2).or(subquery3)
+    end
+    total_records = data.count
+    paginate_records(total_records, data.limit(per_page).offset(page))
+  end
+
+  def show
+    {
+      email: email,
+      username: username,
+      profile_picture_src: profile_picture_src,
+      detail_attributes: detail.attributes
+    }
+  end
+
+  protected
+
+  def profile_picture_src
+    return ActionController::Base.helpers.asset_path('no-profile-picture.png') unless profile_picture.attached?
+
+    Rails.application.routes.url_helpers.rails_blob_path(profile_picture, only_path: true)
+  end
 end
